16ff59a6f8be8b10a7de238c3db4d1b4388a71dd
Rence-L4-Python <145338693+Rence-L4-Python@users.noreply.github.com>
2026-01-14 18:17:18 +0400

    Flowtimer - pt.39 (Responsiveness pt.2)
    > Removed previous mobile nav, instead using the desktop nav, but tweaked for mobile screens.
    > Fixed the account-tooltip in settings.
    > Added a simple loading screen on the working pages so the white screen on page load gets hidden.

--- flowtimer/public/js/main.js
+++ flowtimer/public/js/main.js
@@ -9,3 +9,9 @@ import './crudFeature.js';
 import './audioSFX.js';
 import './popups-toasts.js';
 import './adminRedirect.js';
+
+window.addEventListener('load', () =>{ // added a loading screen because the background image takes time to load
+    setTimeout(() =>{
+        document.getElementById('loadingscreen').classList.add('fade-out')
+    }, 420);
+})
